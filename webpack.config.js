543965468a44421c098a03e188253d6fe0f34879
Butch Bet <butchbet01@gmail.com>
2022-10-29 15:19:35 -0500

    Implement html-webpack-plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
 
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[ext]',
-        open: true;
+        filename: '[name].js',
+        clean: true,
     },
 
     module: {
@@ -21,9 +22,13 @@ module.exports = {
         ]
     },
 
-    plugins: {
-
-    },
+    plugins: [
+        new htmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+    ],
 
     optimization: {
         minimize: true,
