b0e5ab3b0be2b92a5f6949f58b04b659059f1bb4
Butch Bet <butchbet01@gmail.com>
2022-10-29 18:35:42 -0500

    Going ahead with the header.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import '@styles/styles.css';
 import '@styles/tablet.css';
 import '@styles/desktop.css';
-import menuImg from './assets/images/menu.png';
+import menuImg from '@images/menu.png';
 
 // Inserting the image menu image
 document.getElementById('menuImg').src = menuImg;
