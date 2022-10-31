59991bf1316caf49a6c6a681a00d8d42efb09e0c
Butch Bet <butchbet01@gmail.com>
2022-10-31 16:55:42 -0500

    Improved the click event script and add a validation to Check if the nav bar is showed of not.

--- src/index.js
+++ src/index.js
@@ -7,49 +7,68 @@ const putumayoTemplate = require('./components/putumayoTemplate.js');
 const misionCaucheraTemplate = require('./components/misionCaucheraTemplate.js');
 const apaporisTemplate = require('./components/apaporisTemplate.js');
 const culturaIndigenaTemplate = require('./components/culturaIndigenaTemplate.js');
-const planta = require('./components/planta.js');
+const plantaTemplate = require('./components/plantaTemplate.js');
 
 // Inserting the image menu image
 document.getElementById('menuImg').src = menuImg;
 
 // Script to catch the click event in each nav item
 const navigation = Array.from(document.getElementsByClassName('navList__item'));
+const menu = document.getElementById('menuImg');
+const close = document.getElementById('close');
 const components = document.getElementById('components');
+const headerNav = document.getElementById('header__nav');
+components.appendChild(introduccionTemplate.main); // Append the introduction since the begining
+navigation.push(menu);
+navigation.push(close);
 
 navigation.forEach(item => {
-        item.addEventListener('click', (e) => {
-            const target = e.target;
-            
-            switch (target.id) {
-                case 'introduccion':
-                    components.innerHTML = '';
-                    components.appendChild(introduccionTemplate.main);
-                    break;
-            
-                case 'putumayo':
-                    components.innerHTML = '';
-                    components.appendChild(putumayoTemplate.main)
-                    break;
+    item.addEventListener('click', (e) => {
+        const target = e.target;
+        
+        switch (target.id) {
+            case 'introduccion':
+                appendComponent(introduccionTemplate.main);
+                break;
+        
+            case 'putumayo':
+                appendComponent(putumayoTemplate.main);
+                break;
 
-                case 'misionCauchera':
-                    components.innerHTML = '';
-                    components.appendChild(misionCaucheraTemplate.main)
-                    break;
+            case 'misionCauchera':
+                appendComponent(misionCaucheraTemplate.main);
+                break;
 
-                case 'apaporis':
-                    components.innerHTML = '';
-                    components.appendChild(apaporisTemplate.main)
-                    break;
+            case 'apaporis':
+                appendComponent(apaporisTemplate.main);
+                break;
 
-                case 'culturaIndigena':
-                    components.innerHTML = '';
-                    components.appendChild(culturaIndigenaTemplate.main)
-                    break;
+            case 'culturaIndigena':
+                appendComponent(culturaIndigenaTemplate.main);
+                break;
 
-                case 'planta':
-                    components.innerHTML = '';
-                    components.appendChild(planta.main)
-                    break;
-            }
-        });
+            case 'planta':
+                appendComponent(plantaTemplate.main);
+                break;
+
+            case 'menuImg': 
+                headerNav.classList.remove('hidden');
+                break;
+
+            case 'close':
+                headerNav.classList.add('hidden');
+                break;
+                
+        }
     });
+});
+
+
+function appendComponent(component) {
+    if(!headerNav.classList.contains('hidden')) { // Check if the nav bar is showed of not
+        headerNav.classList.add('hidden')
+    }
+
+    components.innerHTML = '';
+    components.appendChild(component);
+}
