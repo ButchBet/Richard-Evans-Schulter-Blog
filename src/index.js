bba27fabfadb9c485e2851526e54389fa08ee4a2
Butch Bet <butchbet01@gmail.com>
2022-10-31 16:17:51 -0500

    Created Script to catch the click event in each nav item

--- src/index.js
+++ src/index.js
@@ -5,3 +5,38 @@ import menuImg from '@images/menu.png';
 
 // Inserting the image menu image
 document.getElementById('menuImg').src = menuImg;
+
+// Script to catch the click event in each nav item
+const navigation = Array.from(document.getElementsByClassName('navList__item'));
+
+navigation.forEach(item => {
+        item.addEventListener('click', (e) => {
+            const target = e.target;
+            
+            switch (target.id) {
+                case 'introduction':
+                    console.log('Clicked the introduction');
+                    break;
+            
+                case 'putumayo':
+                    console.log('clicked putumayo');
+                    break;
+
+                case 'misionCauchera':
+                    console.log('clicked misionCauchera');
+                    break;
+
+                case 'apaporis':
+                    console.log('clicked apaporis');
+                    break;
+
+                case 'culturaIndigena':
+                    console.log('clicked culturaIndigena');
+                    break;
+
+                case 'planta':
+                    console.log('clicked planta');
+                    break;
+            }
+        });
+    });
