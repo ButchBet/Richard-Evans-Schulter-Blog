e112a533ae8715aa69896db78c3b5c30c215619f
Butch Bet <butchbet01@gmail.com>
2022-11-17 12:45:54 -0500

    Finished the introduction template.clear

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import '@styles/styles.css';
 import '@styles/tablet.css';
 import '@styles/desktop.css';
+
 import menuImg from '@images/icons/menu.png';
 import titleImg from "@images/logos/pageLogo2.png";
+import richardImage from "@images/richard.png";
+import firstExplorationsImage from "@images/firstExplorations.jpg";
+import peyoteImage from "@images/peyote.png";
+import mushroomImage from "@images/mushroom.jpg";
+import ololiuquiImage from "@images/ololiuqui.jpg";
+import peyoteZoneImage from "@images/peyoteZone.jpg";
+
 const introduccionTemplate = require('./components/introduccionTemplate.js');
 const putumayoTemplate = require('./components/putumayoTemplate.js');
 const misionCaucheraTemplate = require('./components/misionCaucheraTemplate.js');
@@ -10,18 +18,26 @@ const apaporisTemplate = require('./components/apaporisTemplate.js');
 const culturaIndigenaTemplate = require('./components/culturaIndigenaTemplate.js');
 const plantaTemplate = require('./components/plantaTemplate.js');
 
-// Inserting the image menu image
+const components = document.getElementById('components');
+components.appendChild(introduccionTemplate.main); // Append the introduction since the begining
+
+
+// Inserting the images of the Introduction template
 document.getElementById('menuImg').src = menuImg;
-document.getElementById("titleImg").src = titleImg;
+document.getElementById('titleImg').src = titleImg;
+document.getElementById('richardImage').src = richardImage; 
+document.getElementById('firstExplorations').src = firstExplorationsImage;
+document.getElementById('peyote').src = peyoteImage;
+document.getElementById('mushroom').src = mushroomImage;
+document.getElementById('ololiuqui').src = ololiuquiImage;
+document.getElementById('peyoteZone').src = peyoteZoneImage;
 
 // Script to catch the click event in each nav item
 const navigation = Array.from(document.getElementsByClassName('navList__item'));
 const menu = document.getElementById('menuImg');
 const close = document.getElementById('close');
-const components = document.getElementById('components');
 const option = document.getElementById('option');
 const headerNav = document.getElementById('header__nav');
-components.appendChild(introduccionTemplate.main); // Append the introduction since the begining
 navigation.push(menu);
 navigation.push(close);
 
